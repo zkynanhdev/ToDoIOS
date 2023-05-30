7804570a8a2fb651a017c103c51fe0a1f8905c18
Anh Pham <anhplt12@gmail.com>
2023-05-30 16:02:25 +0700

    Initial Commit

---
+++ first_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first_app
+//
+//  Created by AnhPhamPC on 5/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
